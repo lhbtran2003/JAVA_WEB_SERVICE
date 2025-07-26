bdb927b75b1595a8bbbec67ef46eb59966d28b7a
lhbtran2003 <lhbtran2003@gmail.com>
2025-07-26 23:20:54 +0700

    up

--- Session_9/src/main/java/liliana/session_9/controller/MovieController.java
+++ Session_9/src/main/java/liliana/session_9/controller/MovieController.java
@@ -4,6 +4,7 @@ import liliana.session_9.exception.BadRequestException;
 import liliana.session_9.service.MovieService;
 import liliana.session_9.entity.Movie;
 import liliana.session_9.model.MovieDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
+@Slf4j
 public class MovieController {
     @Autowired
     private MovieService movieService;
@@ -19,10 +23,12 @@ public class MovieController {
     @PostMapping("/movies")
     public ResponseEntity<Movie> addMovie(@RequestBody MovieDto request) throws BadRequestException {
         if (request.getTitle() == null || request.getTitle().isBlank()) {
+            log.error("Invalid title: {}", request.getTitle());
             throw new BadRequestException("Tiêu đề phim không được để trống");
         }
-
-        return new ResponseEntity<Movie>( movieService.add(request), HttpStatus.CREATED);
+        Movie movie = movieService.add(request);
+        log.info("Add movie successfully: {}, lúc: {}", movie.getTitle(), LocalDateTime.now());
+        return new ResponseEntity<Movie>( movie, HttpStatus.CREATED);
 
     }
 }
